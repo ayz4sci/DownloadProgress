7a50b94f458809de60e88b02463629dced7abacb
aajebeku <ayz4sci@yahoo.com>
2015-06-30 11:56:48 +0100

    Fully implemented

---
+++ app/src/main/java/com/ayz4sci/downloadprogressexample/MainActivity.java
@@ -0,0 +1,119 @@
+package com.ayz4sci.downloadprogressexample;
+
+import android.app.DownloadManager;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.ayz4sci.downloadprogressview.DownloadProgressView;
+
+import java.io.File;
+
+
+/**
+ * Created by Ayoola Ajebeku on 6/30/15.
+ */
+public class MainActivity extends ActionBarActivity implements DownloadProgressView.DownloadStatusListener {
+
+    private Button downloadButton, playButton;
+    private long downloadID;
+    private String downloadURL = "http://bit.ly/1g5BfyR";
+    private DownloadManager downloadManager;
+    private DownloadProgressView downloadProgressView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        downloadManager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+
+        downloadProgressView = (DownloadProgressView) findViewById(R.id.downloadProgressView);
+        downloadButton = (Button) findViewById(R.id.downloadButton);
+        downloadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DownloadManager.Request request = new DownloadManager.Request(Uri.parse(downloadURL));
+                request.setTitle("You garrit");
+                request.setDescription("DownloadProgress sample");
+                request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                request.setDestinationInExternalFilesDir(getApplicationContext(), null, "you_garrit.mp4");
+                request.allowScanningByMediaScanner();
+
+                downloadID = downloadManager.enqueue(request);
+                downloadProgressView.show(downloadID, MainActivity.this);
+                downloadButton.setVisibility(View.GONE);
+            }
+        });
+
+        playButton = (Button) findViewById(R.id.playButton);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setDataAndType(Uri.fromFile(getFileUri()), "video/*");
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public File getFileUri() {
+        DownloadManager.Query query = new DownloadManager.Query();
+        query.setFilterById(downloadID);
+        Cursor cur = downloadManager.query(query);
+
+        if (cur.moveToFirst()) {
+            int columnIndex = cur.getColumnIndex(DownloadManager.COLUMN_STATUS);
+            if (DownloadManager.STATUS_SUCCESSFUL == cur.getInt(columnIndex)) {
+                String uriString = cur.getString(cur.getColumnIndex(DownloadManager.COLUMN_LOCAL_URI));
+                return new File(Uri.parse(uriString).getPath());
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public void downloadFailed(int reason) {
+        downloadButton.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void downloadSuccessful() {
+        playButton.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void downloadCancelled() {
+        downloadButton.setVisibility(View.VISIBLE);
+    }
+}
---
+++ downloadprogress/src/main/java/com/ayz4sci/downloadprogressview/DownloadProgressView.java
@@ -0,0 +1,254 @@
+package com.ayz4sci.downloadprogressview;
+
+import android.app.DownloadManager;
+import android.content.Context;
+import android.content.res.ColorStateList;
+import android.content.res.TypedArray;
+import android.database.Cursor;
+import android.graphics.Color;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Ayoola Ajebeku on 6/29/15.
+ */
+public class DownloadProgressView extends LinearLayout {
+
+    private final ProgressBar downloadProgressBar;
+    private final TextView downloadedSizeView, totalSizeView, percentageView;
+    private final Button cancelButton;
+    private final DownloadManager downloadManager;
+    private final Context context;
+    private int downloadedSizeColor, totalSizeColor, percentageColor;
+    private long downloadID;
+    private boolean downloading;
+    private List<DownloadStatusListener> listeners = new ArrayList<DownloadStatusListener>();
+
+    public DownloadProgressView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.context = context;
+
+        TypedArray typedArray = context.getTheme().obtainStyledAttributes(attrs, R.styleable.DownloadProgressView, 0, 0);
+
+        try {
+            downloadedSizeColor = typedArray.getColor(R.styleable.DownloadProgressView_downloadedSizeColor, Color.BLACK);
+            totalSizeColor = typedArray.getColor(R.styleable.DownloadProgressView_totalSizeColor, Color.BLACK);
+            percentageColor = typedArray.getColor(R.styleable.DownloadProgressView_totalSizeColor, Color.BLACK);
+        } finally {
+            typedArray.recycle();
+        }
+
+        setOrientation(LinearLayout.VERTICAL);
+        setGravity(Gravity.CENTER);
+
+        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        layoutInflater.inflate(R.layout.download_progres_view, this, true);
+
+        downloadManager = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
+
+        downloadedSizeView = (TextView) findViewById(R.id.downloadedSize);
+        totalSizeView = (TextView) findViewById(R.id.totalSize);
+        percentageView = (TextView) findViewById(R.id.percentage);
+        downloadProgressBar = (ProgressBar) findViewById(R.id.downloadProgressBar);
+
+        downloadedSizeView.setTextColor(ColorStateList.valueOf(percentageColor));
+        totalSizeView.setTextColor(ColorStateList.valueOf(percentageColor));
+        percentageView.setTextColor(ColorStateList.valueOf(percentageColor));
+
+        cancelButton = (Button) findViewById(R.id.cancelButton);
+        cancelButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (downloadManager != null) {
+                    downloadManager.remove(downloadID);
+                    try {
+                        for (DownloadStatusListener downloadStatusListener : listeners) {
+                            downloadStatusListener.downloadCancelled();
+                        }
+                    } catch (Exception e) {
+                    }
+                }
+                setVisibility(View.GONE);
+            }
+        });
+
+        //hides view.
+        setVisibility(View.GONE);
+    }
+
+    /**
+     * This method sets the color of the downloadedSize TextView.
+     *
+     * @param downloadedSizeColor the color of the downloadedSize TextView.
+     */
+    public void setDownloadedSizeColor(int downloadedSizeColor) {
+        this.downloadedSizeColor = downloadedSizeColor;
+        downloadedSizeView.setTextColor(ColorStateList.valueOf(percentageColor));
+        invalidate();
+        requestLayout();
+    }
+
+    /**
+     * This method sets the color of the totalSize TextView.
+     *
+     * @param totalSizeColor the color of the totalSize TextView.
+     */
+    public void setTotalSizeColor(int totalSizeColor) {
+        this.totalSizeColor = totalSizeColor;
+        totalSizeView.setTextColor(ColorStateList.valueOf(percentageColor));
+        invalidate();
+        requestLayout();
+    }
+
+    /**
+     * This method sets the color of the percentage TextView.
+     *
+     * @param percentageColor the color of the percentage TextView.
+     */
+    public void setPercentageColor(int percentageColor) {
+        this.percentageColor = percentageColor;
+        percentageView.setTextColor(ColorStateList.valueOf(percentageColor));
+        invalidate();
+        requestLayout();
+    }
+
+    /**
+     * This method returns the color of the downloadedSize TextView.
+     *
+     * @return the color of the downloadedSize TextView.
+     */
+    public int getDownloadedSizeColor() {
+        return downloadedSizeColor;
+    }
+
+    /**
+     * This method returns the color of the totalSize TextView.
+     *
+     * @return the color of the totalSize TextView.
+     */
+    public int getTotalSizeColor() {
+        return totalSizeColor;
+    }
+
+    /**
+     * This method returns the color of the percentage TextView.
+     *
+     * @return the color of the percentage TextView.
+     */
+    public int getPercentageColor() {
+        return percentageColor;
+    }
+
+    /**
+     * This method initialize and shows the downloadProgressView.
+     *
+     * @param downloadID             the downloadID gotten when a download was enqueued.
+     * @param downloadStatusListener the downloadStatusListener to monitor when
+     *                               download is successful, failed, cancelled.
+     */
+    public void show(long downloadID, DownloadStatusListener downloadStatusListener) {
+        this.downloadID = downloadID;
+        listeners.add(downloadStatusListener);
+        showDownloadProgress();
+    }
+
+
+    private void showDownloadProgress() {
+        setVisibility(View.VISIBLE);
+
+        new Thread() {
+            @Override
+            public void run() {
+                do {
+                    downloading = true;
+
+                    final Cursor c;
+                    DownloadManager.Query query = new DownloadManager.Query();
+                    query.setFilterById(downloadID);
+                    c = downloadManager.query(query);
+                    if (c.moveToFirst()) {
+                        final int status = c.getInt(c.getColumnIndex(DownloadManager.COLUMN_STATUS));//Get download status
+                        final int reason = c.getInt(c.getColumnIndex(DownloadManager.COLUMN_REASON));//Get download status
+                        final long bytes_downloaded = c.getInt(c.getColumnIndex(DownloadManager.COLUMN_BYTES_DOWNLOADED_SO_FAR));
+                        final long bytes_total = c.getInt(c.getColumnIndex(DownloadManager.COLUMN_TOTAL_SIZE_BYTES));
+                        final long download_percentage = (bytes_downloaded * 100l) / bytes_total;
+
+                        new Handler(Looper.getMainLooper()).post(new Runnable() {
+                            @Override
+                            public void run() {
+                                if (status == DownloadManager.STATUS_RUNNING) {
+                                    downloading = true;
+                                    downloadProgressBar.setIndeterminate(false);
+                                    downloadedSizeView.setText(String.format("%.3fMB", ((bytes_downloaded * 1.0) / 1024l / 1024l)));
+                                    totalSizeView.setText(String.format("%.2fMB", ((bytes_total * 1.0) / 1024l / 1024l)));
+                                    percentageView.setText((int) download_percentage + "%");
+                                    downloadProgressBar.setProgress((int) download_percentage);
+                                } else if (status == DownloadManager.STATUS_FAILED) {
+                                    downloading = false;
+                                    setVisibility(View.GONE);
+
+                                    try {
+                                        for (DownloadStatusListener downloadStatusListener : listeners) {
+                                            downloadStatusListener.downloadFailed(reason);
+                                        }
+                                    } catch (Exception e) {
+                                    }
+                                } else if (status == DownloadManager.STATUS_SUCCESSFUL) {
+                                    downloading = false;
+                                    setVisibility(View.GONE);
+
+                                    for (DownloadStatusListener downloadStatusListener : listeners) {
+                                        downloadStatusListener.downloadSuccessful();
+                                    }
+                                } else {
+                                    downloading = true;
+                                    downloadedSizeView.setText("");
+                                    totalSizeView.setText("");
+                                    percentageView.setText("");
+                                    downloadProgressBar.setIndeterminate(true);
+                                }
+                            }
+                        });
+                    } else {
+                        new Handler(Looper.getMainLooper()).post(new Runnable() {
+                            @Override
+                            public void run() {
+                                downloading = false;
+                                setVisibility(View.GONE);
+
+                                try {
+                                    for (DownloadStatusListener downloadStatusListener : listeners) {
+                                        downloadStatusListener.downloadFailed(-1);
+                                    }
+                                } catch (Exception e) {
+                                }
+                            }
+                        });
+                    }
+                    c.close();
+                } while (downloading);
+            }
+        }.start();
+    }
+
+    public interface DownloadStatusListener {
+        public void downloadFailed(int reason);
+
+        public void downloadSuccessful();
+
+        public void downloadCancelled();
+
+    }
+}
